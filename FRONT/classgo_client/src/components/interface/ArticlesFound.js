9d815ca31a3beec41a1b30a0e7b008a0ccc9f4aa
stambouli <aminestambouli90@yahoo.fr>
2021-03-12 21:24:25 +0100

    Add articles filter

--- FRONT/classgo_client/src/components/interface/ArticlesFound.js
+++ FRONT/classgo_client/src/components/interface/ArticlesFound.js
@@ -4,24 +4,25 @@ import React from 'react';
 const ArticlesFound = (props) => {
   const { articles } = props.data;
   const { setArticles } = props.data;
-  // const [category, setCategory] = useState(1);
+  const { category } = props.data;
+  const { setCategory } = props.data;
   const selectCategory = (event) => {
-    const category = event.target;
-    // setCategory(value);
-    setArticles(articles.filter((item) => item.category_id === category));
-    console.log('mes categories', category);
+    const id = parseInt(event.target.id, 10);
+    setCategory(id);
+    setArticles((prevState) => prevState.filter((item) => item.category_id === category));
   };
   return articles !== null ? (
     <div className="articlesFound">
       <div className="categoryArticles">
-        <h4 aria-hidden value="1" onClick={selectCategory}>Montres</h4>
-        <h4 aria-hidden value="2" onClick={selectCategory}>Lunettes</h4>
-        <h4 aria-hidden value="3" onClick={selectCategory}>Bijoux</h4>
+        <h4 aria-hidden onClick={() => setCategory('')}>tous</h4>
+        <h4 aria-hidden id="1" onClick={selectCategory}>Montres</h4>
+        <h4 aria-hidden id="2" onClick={selectCategory}>Lunettes</h4>
+        <h4 aria-hidden id="3" onClick={selectCategory}>Bijoux</h4>
       </div>
       <div className="allArticles">
         {
          articles.map((article) => (
-           <div className="article-container">
+           <div key={article.article_id} className="article-container">
              <img src={article.picture} alt="montre" />
              <h2>{article.name}</h2>
              <div className="starsAndPrice">
--- FRONT/classgo_client/src/components/interface/Footer.js
+++ FRONT/classgo_client/src/components/interface/Footer.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Footer = () => (
-  <div className="footer">FOOTER</div>
-);
+const Footer = () => {
+  const [email, setEmail] = useState('');
+  const handleEmail = (event) => {
+    const { value } = event.target;
+  };
+  return (
+    <div>
+      <p>Inscrivez vous pour recevoir nos differentes offres, promotions et avantages client</p>
+      <input type="text" id="newLatter" placeholder="Entrez votre adresse email" value={email} />
+      <button type="button">INSCRIVEZ-VOUS</button>
+    </div>
+  );
+};
 
 export default Footer;
