5843cf20af7ebf861bcfa24391ee4c7bd383de87
stambouli <aminestambouli90@yahoo.fr>
2021-03-15 11:45:31 +0100

    Add routerNewsletters

--- BACK/classgo_api/index.js
+++ BACK/classgo_api/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const rateLimit = require('express-rate-limit');
 
 const app = express();
 const cors = require('cors');
@@ -10,22 +9,6 @@ const routerImage = require('./routes/images');
 const routerNewsletters = require('./routes/newsletters');
 
 const PORT = 5000 || process.env.PORT;
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-//   );
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   next();
-// });
-
-// const limiter = rateLimit({
-//   windowMs: 3 * 60 * 1000,
-//   max: 30, // per IP
-// });
-// app.use(limiter);
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
