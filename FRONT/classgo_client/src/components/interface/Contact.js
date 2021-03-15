0af3e2183130d48827cda01b15241bac484f8bc0
stambouli <aminestambouli90@yahoo.fr>
2021-03-15 14:11:59 +0100

    Add Contact form

--- FRONT/classgo_client/src/components/interface/Contact.js
+++ FRONT/classgo_client/src/components/interface/Contact.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
 const Contact = () => (
-  <div>
-    CONTACT
+  <div className="contact">
+    <p>CONTACTEZ NOUS</p>
+    <div className="emailName">
+      <input type="text" id="input1" placeholder="Nom" />
+      <input type="text" id="input2" placeholder="Email" />
+    </div>
+    <div className="object">
+      <input type="text" placeholder="Objet" />
+    </div>
+    <div className="message">
+      <textarea type="text" rows="10" lang="10" placeholder="Message" />
+    </div>
+    <div className="btn">
+      <button type="button" id="btn1">Envoyer</button>
+      <button type="button" id="btn2">Réinitialiser</button>
+    </div>
   </div>
 );
 
